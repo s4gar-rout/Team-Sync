e1784b415c8264e74a044a8ba91dfb54056cd920
Sagar <sagarrout599@gmail.com>
2026-09-10 15:55:58 +0530

    feat(auth): add user model and password hashing

--- Server/src/features/auth/auth.model.js
+++ Server/src/features/auth/auth.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
     {
@@ -34,6 +35,21 @@ const userSchema = new mongoose.Schema(
         timestamps: true
     }
 );
+
+// Hash password before saving
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) {
+        return;
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Compare plain password with hashed password
+userSchema.methods.comparePassword = async function (plainPassword) {
+    return bcrypt.compare(plainPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
